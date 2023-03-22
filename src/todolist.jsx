0100b08d02354afe39b740538bb6bdd96cae71f0
gourav088 <thakurgourav124421@gmail.com>
2023-03-22 17:32:03 +0530

    fetchApi

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -20,6 +20,15 @@ const TodoApp = () => {
     // Second way -> it may create stale closure issue with async to multi state
     //setTodos([...todos, todoTitle]);
   };
+  const onDeleteTodo = (id) => {
+    console.log(id, "check id");
+    setList((prevList) => {
+      const Printresult = prevList.filter((item, index) => index !== id);
+      console.log(Printresult, "result");
+      return Printresult;
+    });
+  };
+
 
   // Define return for JSX
   return (
@@ -40,6 +49,15 @@ const TodoApp = () => {
             return <li key={index}>{item}</li>;
           })}
         </ul>
+        <span>
+        {list.map((data, index) => {
+          return (
+            <div key={index}>
+              {data} <button onClick={() => onDeleteTodo(index)}>delete</button>
+            </div>
+          );
+        })}
+      </span>
       </div>
     </>
   );
